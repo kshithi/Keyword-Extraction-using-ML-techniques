b1e5bd6a28e5f7c7db382bf1073a8440ebef8b9a
kshithija147@gmail.com <kshithija147@gmail.com>
2021-01-01 20:13:38 -0800

    initial textrank

---
+++ textRank.py
@@ -0,0 +1,22 @@
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+import trafilatura
+array_links = [
+    "https://en.wikipedia.org/wiki/Coronavirus_disease_2019",
+    "https://en.wikipedia.org/wiki/Recession",
+    "https://en.wikipedia.org/wiki/Vienna",
+    "https://en.wikipedia.org/wiki/Machine_learning",
+    "https://en.wikipedia.org/wiki/Graph_database"
+]
+array_text = []
+for l in array_links:
+    html = trafilatura.fetch_url(l)
+    text = trafilatura.extract(html)
+    text_clean = text.replace("\n", " ").replace("\'", "")
+    array_text.append(text_clean[0:5000])
+
+from summa import keywords
+for j in range(len(array_text)):
+    print("Keywords of article", str(j+1), "\n", (keywords.keywords(array_text[j], words=5)).split("\n"))
